0f0d6309f9ec3036ee50055d8925821935d419b0
Vivian Quach <vquach@ucsd.edu>
2021-03-03 23:16:07 -0800

    fixed settings route

--- routes/settings.js
+++ routes/settings.js
@@ -1,4 +1,18 @@
-
 exports.view = function(req, res){
-  	res.render('settings');
- };
+  res.render('settings', {
+	'faq': [
+		{ 	'question': 'What is WeStudy?',
+		    'id': 'faq1'
+		},
+		{ 	'question': 'Is WeStudy free?',
+		    'id': 'faq2'
+		},
+		{ 	'question': 'What can I do with WeStudy?',
+		    'id': 'faq3'
+		},
+		{ 	'question': "How can I check my friends' progress?",
+		    'id': 'faq4'
+		}   	
+	]
+  });
+};
