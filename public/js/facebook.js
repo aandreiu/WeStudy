91ca6e73096aea36fdf76e92316df4ef39328405
Alexandra Andreiu <aandreiu@ucsd.edu>
2021-03-04 10:21:32 -0800

    fb login

--- public/js/facebook.js
+++ public/js/facebook.js
@@ -11,6 +11,7 @@ function statusChangeCallback(response) {
   if (response.status === 'connected') {
     // Logged into your app and Facebook.
         console.log('Successfully logged in with Facebook');
+        FB.api('/me?fields=name,first_name,picture.width(480)', changeUser);
         window.location = "/index";
   }
 }
